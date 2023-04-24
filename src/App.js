bc9281dfab865310920fb1b15be922e37dab53d9
Raghav-007 <108212552+Raghav-007@users.noreply.github.com>
2023-04-24 14:53:45 +0530

    React js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <iframe
+	src="https://raghav001-doctalk.hf.space"
+	frameborder="0"
+	width="100%"
+	// height="100vh"
+  style={{height: "100vh"}}
+></iframe>
+
+  </>
+  );
+}
+
+export default App;
